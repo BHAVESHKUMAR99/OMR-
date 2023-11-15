af3c30d48969f7580395296a0577aa09b1306faf
darkkiller <bhaveshkumarbk1999@gmail.com>
2023-11-15 15:30:58 +0530

    Add on git

---
+++ omrs/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+<title>Online Marriage Regitration System :: Home Page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Online Marriage Registration System . Register Free to find your Perfect Match" />
+      <meta name="keywords" content="Online Marriage Registration System :: Home Page" />
+      <meta name="abstract" content="Online Marriage Registration System - Worlds No1. ISO certified Marriage Portal for Indian Singles." />
+      <meta name="Author" content="Online Marriage Registration System Team" />
+      <meta name="copyright" content="2022 Online Marriage Registration System" />
+
+      <meta name="google-site-verification" content="FZ-QVx2tmuYqd9yEIMc-aiRSK7Cc2QgH9agtXNS4mog" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--//Meta tag Keywords -->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/><!--stylesheet-->
+	<link rel="stylesheet" href="css/font-awesome.css"><!--font_aswesome_icons-->
+	<link href="//fonts.googleapis.com/css?family=Basic" rel="stylesheet"><!--online-fonts-->
+	<link href="//fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet"><!--online-fonts-->
+
+	<!-- METRO -->
+	<meta name="msApplication-ID" content="App"/>
+      <meta name="msApplication-PackageFamilyName" content="bbadbd78-7573-4ded-a85d-64d256b2e05e"/>
+      <meta http-equiv="cache-control" content="no-cache" />
+      <meta http-equiv="pragma" content="no-cache" />
+      <link rel="canonical" href="http://localhost/om/omrs/" />
+      <link  rel="shortcut icon" type="image/x-icon" href="images/logo.png" />
+      <link rel=icon type="image/ico" href="images/logo.png" />
+            <link rel="stylesheet" type="text/css" href="//imgs.bharatmatrimony.com/bmstyles/bm-hp-desktop.css?v30" />
+      <style>.logotop{top:70px !important;}</styl>
+            <!--[if IE 8 ]>    <html lang="en" class="ie8">    <![endif]-->
+      <!--[if IE 9 ]>    <html lang="en" class="ie9">    <![endif]-->
+
+            
+      <style>
+
+         .matrimony-logo-image {
+			background: url(images/logo.png) no-repeat;
+            width: 150px;
+            height: 150px;
+            background-size: cover;
+
+         }
+
+       
+      </style>
+
+</head>
+<body>
+	<center>
+   <div class="hpmainwraper">
+   <div class="pos-relative hp-wrapper">
+   <div class="logotop">
+   <div itemscope itemtype="http://schema.org/ImageObject">
+     <br><br><br> 
+      <div class="clickable-block" data-hyperlink="http://localhost/om/omrs/" data-navtype="current">
+         <div class="matrimony-logo-image"> </div>
+      </div>
+   </div>
+   </div>
+   </div>
+
+ <!-- Registration Part Start-->		
+ <div class="new-replace-bharat"> 
+   <div class="mob-padd">
+   <!-- Navigation -->
+   <div onclick="regPDLogTrackAjax('header-part')" style="width: 100%;">
+   <div class="nav-bg-wraper1" style="box-shadow: 0 1px 6px -2px #999;">
+   <div class="innerwrapper" style="width: 1000px;">
+   <ul class="hdr-link" style="list-style:none;">
+  <!-- <li style="margin-left:0px;font-family=""><a title="Assamese Matrimony" href="http://localhost/om/omrs/">Assamese</a></li>
+   <li><a title="Bengali Matrimony" href="http://localhost/om/omrs/">Bengali</a></li>
+   <li><a title="Bihari Matrimony" href="http://localhost/om/omrs/">Bihari</a></li>
+   <li><a title="Gujarati Matrimony" href="http://localhost/om/omrs/">Gujarati</a></li>
+   <li><a title="Hindi Matrimony" href="http://localhost/om/omrs/">Hindi</a></li>
+   <li><a title="Kannada Matrimony" href="http://localhost/om/omrs/">Kannada</a></li>
+   <li><a title="Kerala Matrimony" href="http://localhost/om/omrs/">Kerala</a></li>
+   <li><a title="Marathi Matrimony" href="http://localhost/om/omrs/">Marathi</a></li>
+   <li><a title="Marwadi Matrimony" href="http://localhost/om/omrs/">Marwadi</a></li>
+   <li><a title="Oriya Matrimony" href="http://localhost/om/omrs/">Oriya</a></li>
+   <li><a title="Parsi Matrimony" href="http://localhost/om/omrs/">Parsi</a></li>
+   <li><a title="Punjabi Matrimony" href="http://localhost/om/omrs/">Punjabi</a></li>
+   <li><a title="Rajasthani Matrimony" href="http://localhost/om/omrs/">Rajasthani</a></li>
+      --> </ul>
+   </div>
+   </div>
+   </div>
+
+   <div class="w3ls-icons">
+		<ul style="font-size: 30px">
+      <li><a href="http://localhost/om/omrs/user/signup.php" style="color: blue;padding-right: 40px;margine:30px">  User Registration Form     </a></li>
+			<li><a href="admin/login.php" style="color: blue;">  Admin</a></li>
+			<li style="padding-left: 20px"><a href="user/login.php" style="color: blue;"> User</a></li>
+		</ul>
+	</div>
+   <br><br><br><br>
+   <div class="w3ls-head">
+		<h1> Online Marriage Registration System</h1>
+
+</div><br><br>
+<div class="w3ls-content">
+	<div class="w3ls-headding">
+		<h2> <img src="images/5b9d320a2000002d00fdde0f.jpeg"></h2>
+		<p>We are launching the most awesome site ever. It's gonna be legendary</p>
+	</div>
+	
+	
+</div>
+	
+<br><br>
+<footer style="color:brown">&copy; 2022 Online Marriage Registration System</footer>
+</body>
+</html>
